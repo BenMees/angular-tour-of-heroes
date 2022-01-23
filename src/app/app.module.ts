77a7191778fd7f5bdb3413386ea2fb10ccdeb27b
Ben Mees <benmees@outlook.com>
2022-01-23 10:47:26 +0100

    set all on github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 import { MessagesComponent } from './messages/messages.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 // import { HeroDetailComponent} from "./hero-detal/hero-detal.component";
 
@@ -15,7 +16,8 @@ import {AppRoutingModule} from "./app-routing.module";
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
